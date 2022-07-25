c3d992ab3378d3f4acfad699cd6becb394c45e81
Stefan Göllner <goellnor@gmail.com>
2022-07-25 17:11:31 +0200

    algolia

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Laravel\Scout\Searchable;
+
 
 class Ingredient extends Model
 {
     use HasFactory;
+    use Searchable;
 
     /**
      * The attributes that are mass assignable.
